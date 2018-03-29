1be5b89e02adb3466b8012525681db3d34f380d6
Yi-Pei, Tu <f2wdulm@gmail.com>
2018-03-29 14:57:16 +0200

    semantic ui

---
+++ job-app/src/JobItem/JobItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './JobItem.css';
+import { Link } from 'react-router-dom';
+
+class JobItem extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="JobItem">
+       	
+      </div>
+    );
+  }
+}
+
+export default JobItem;
