39ebc9d88fa2bfba85949067cf20c5832b8dc130
Yi-Pei, Tu <f2wdulm@gmail.com>
2018-04-25 10:45:53 +0200

    pass data to NetVis

--- job-app/src/components/NetVis.js
+++ job-app/src/components/NetVis.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 // import './JobList.css';
+import PropTypes from 'prop-types'
 
 class NetVis extends Component {
 
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    relatedData: PropTypes.object.isRequired
+  }
+
   render() {
+  	const {data, relatedData} = this.props
     return (
       <div className="NetVis">
        	
